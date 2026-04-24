9866482cb4b16f7bca6a1e273d9da661f9db9cf5
Renato Oliveira <renatoj.oliveiracruz@gmail.com>
2026-04-24 16:52:35 -0300

    feat: EX2/EX02 resolution

---
+++ 03-Javascript/Exercicios/Exercicio-2/script.js
@@ -0,0 +1,25 @@
+// EX 01
+
+let motocyclicleSpeed = 299;
+let carSpeed = 279;
+
+if (motocyclicleSpeed > carSpeed) {
+    console.log("A motocicleta é mais rápida que o carro");
+} else if (carSpeed > motocyclicleSpeed) {
+    console.log("O carro é mais rápido que a motocicleta");
+}
+
+// EX 02
+let atacante = prompt("Digite o nome do atacante");
+let defensor = prompt("Digite o nome do defensor");
+let shield = confirm("O defensor tem escudo? (sim/não)");
+let atk = prompt("Digite o poder do ataque");
+let def = prompt("Digite o  da defesa");
+
+if (atk > def && !shield) {
+    console.log(atk - def + " de dano! causado" + atacante + " tem a vantagem!");
+} else if (def > atk && shield) {
+    console.log(atk - def / 2 + " de dano causado! " + defensor + " conseguiu se defender com o escudo!");
+}  else {
+    console.log(atacante + " não causou dano algum, pois o " + defensor + " esta bem protegido!");
+};
