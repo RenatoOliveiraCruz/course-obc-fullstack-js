d7b81cf08a996c44c1e96ffa4358c86e10a28c8b
Renato Oliveira <renatoj.oliveiracruz@gmail.com>
2026-05-07 11:03:14 -0300

    feat: EX3/script.js/let switch

--- 03-Javascript/Exercicios/Exercicio-3/script.js
+++ 03-Javascript/Exercicios/Exercicio-3/script.js
@@ -1,1 +1,24 @@
 let valorMetro = parseFloat(prompt("Digite o valor em metro:"));
+
+switch (true) {
+    case 1: 
+        alert("O valor em milímetros é: " + valorMetro * 1000 + " mm");
+        break;
+    case 2:
+        alert("O valor em centímetros é: " + valorMetro * 100 + " cm");
+        break;
+    case 3:
+        alert("O valor em decímetros é: " + valorMetro * 10 + " dm");
+        break;
+    case 4:
+        alert("O valor em decâmetros é: " + valorMetro / 10 + " dam");
+        break;
+    case 5:
+        alert("O valor em hectômetros é: " + valorMetro / 100 + " hm");
+        break;
+    case 6:
+        alert("O valor em quilômetros é: " + valorMetro / 1000 + " km");
+        break;
+    default:
+        alert("Opção inválida!");
+}
