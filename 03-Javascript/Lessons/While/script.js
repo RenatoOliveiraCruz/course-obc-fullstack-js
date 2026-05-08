a347d13fcc67ad7f5ac7840d8d1f5c5486b060cc
Renato Oliveira <renatoj.oliveiracruz@gmail.com>
2026-05-08 15:07:00 -0300

    fix: 03/While/script.js att

---
+++ 03-Javascript/Lessons/While/script.js
@@ -0,0 +1,13 @@
+let speed = 80;
+
+while (speed > 0) {
+    alert("A velocidade atual é: " + speed + " km/h");
+    speed -= 20;
+    alert("Reduzindo 20 km/h");
+
+    if (speed = 40) {
+        break;
+    };
+};
+
+alert("O veículo parou!");
