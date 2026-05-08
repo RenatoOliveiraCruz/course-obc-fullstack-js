1e31243a181ad90727678f48fd9b63349f3883e1
Renato Oliveira <renatoj.oliveiracruz@gmail.com>
2026-05-08 15:33:29 -0300

    feat: 03/EX04/script.js second try

--- 03-Javascript/Exercicios/Exercicio-4/script.js
+++ 03-Javascript/Exercicios/Exercicio-4/script.js
@@ -1,18 +1,12 @@
 let turistName = prompt("Digite o nome do turista:");
-
-let pergunta;
 let cidadesVisitadas = 0;
-while (pergunta === "sim") {
-    pergunta = prompt("O turista " + turistName + " visitou alguma cidade? (Sim/Não)").toLowerCase();
+let pergunta = prompt("O turista " + turistName + " visitou alguma cidade? (Sim/Não)").toLowerCase();
+
+if (pergunta === "sim") {
     let cidade = prompt("Digite o nome da cidade visitada:");
     let nomesCidades = "Todas as cidades visitadas por " + turistName + " são: " + cidade + ", ";
     cidadesVisitadas++;
-if (pergunta === "não") {
+} else { 
         alert("O turista " + turistName + " visitou " + cidadesVisitadas + " cidade(s).");
         alert(nomesCidades);   
-}
- // for (let i = 0; i < cidadesVisitadas; i++) {
-    //     let cidade = prompt("Digite o nome da cidade visitada:");
-    //     nomesCidades.push(cidade);
-    // }
-}
+ }
