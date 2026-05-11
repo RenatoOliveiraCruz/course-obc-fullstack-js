150fc56ab10b47c8c62121f36019d094b3b0f23d
Renato Oliveira <renatoj.oliveiracruz@gmail.com>
2026-05-11 08:53:51 -0300

    feat: Lessons/Do-While/index.html & script.js

---
+++ 03-Javascript/Lessons/Do-While/script.js
@@ -0,0 +1,9 @@
+let speed = 40;
+
+do {
+    alert("A velocidade atual é: " + speed + " km/h");
+    speed -= 20;
+} while (speed > 0);
+
+alert("Velocidade final: " + speed + " km/h");
+alert("O veículo parou!");
