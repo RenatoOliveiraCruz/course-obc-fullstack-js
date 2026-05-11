a6f9c23c0170672b29f00f1c7f58c0ca53c91c4a
Renato Oliveira <renatoj.oliveiracruz@gmail.com>
2026-05-11 09:02:49 -0300

    feat: EX/EX05/menu

---
+++ 03-Javascript/Exercicios/Exercicio-5/script.js
@@ -0,0 +1,25 @@
+let menu;
+
+do {
+    menu = prompt(parseFloat("Escolha uma opção:\n1 - Iniciar Jogo\n2 - Carregar Jogo\n3 - Configurações\n4 - Créditos\n5 - Sair"));
+    switch (menu) {
+        case "1":
+            alert("Iniciando jogo...");
+            break;
+        case "2":
+            alert("Carregando jogo...");
+            break;
+        case "3":
+            alert("Abrindo configurações...");
+            break;
+        case "4":
+            alert("Mostrando créditos...");
+            break;
+        case "5":
+            alert("Saindo...");
+            break;
+        default:
+            alert("Opção inválida, tente novamente.");
+        }
+    } while (menu !== "5");
+
