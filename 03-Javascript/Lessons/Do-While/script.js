01c6d68d9253ffbc029c1a61d26c1301db6a96ce
Renato Oliveira <renatoj.oliveiracruz@gmail.com>
2026-05-11 08:55:01 -0300

    fix: Lessons/Do-While/index.html & script.js

--- 03-Javascript/Lessons/Do-While/script.js
+++ 03-Javascript/Lessons/Do-While/script.js
@@ -3,7 +3,7 @@ let speed = 40;
 do {
     alert("A velocidade atual é: " + speed + " km/h");
     speed -= 20;
-} while (speed > 0);
+} while (speed >= 10);
 
 alert("Velocidade final: " + speed + " km/h");
 alert("O veículo parou!");
