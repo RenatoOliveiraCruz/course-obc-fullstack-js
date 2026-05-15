1db8134389729941b951dc8aad689d34d327da26
RenatoOliveiraCruz <renatoj.oliveiracruz@gmail.com>
2026-05-15 20:41:27 -0300

    feat: 03/Arrays slice

--- 03-Javascript/Lessons/Arrays/script.js
+++ 03-Javascript/Lessons/Arrays/script.js
@@ -11,14 +11,39 @@ console.log(array2);
 
 const arr = ["Homer", "Marge", "Bart", "Lisa", "Maggie"];
 
+// push: Adiciona um elemento no final do array e devolve o novo tamanho do array
 let tamanho = arr.push("Apu");
 console.log(arr);
 console.log(tamanho);
 
+// unshift: Adiciona um elemento no começo do array e devolve o novo tamanho do array
 tamanho = arr.unshift("Apu");
 console.log(arr);
 console.log(tamanho);
 
+// pop: Remove um elemento no final do array e devolve o elemento removido
 const lastElement = arr.pop();
 console.log(arr);
 console.log(lastElement);
+
+// shift: Remove um elemento no começo do array e devolve o elemento removido
+const firstElement = arr.shift();
+console.log(arr);
+console.log(firstElement);
+
+// includes: Verifica se um certo elemento está presente no array
+const hasBart = arr.includes("Bart");
+console.log(hasBart);
+const hasApu = arr.includes("Apu");
+console.log(hasApu);
+
+// indexOf: Encontra o índice que possui o elemento indicado, ou -1 caso ele não exista no array
+const indexOfLisa = arr.indexOf("Lisa");
+console.log(indexOfLisa);
+const indexOfApu = arr.indexOf("Apu");
+console.log(indexOfApu);
+
+// slice: Copia uma parte do array e devolve a parte copiada sem alterar o array original
+const slicedArray = arr.slice(0, 4);
+console.log(arr);
+console.log(slicedArray);
