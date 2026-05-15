99d637506d534858c4d763b86116089c732b936f
RenatoOliveiraCruz <renatoj.oliveiracruz@gmail.com>
2026-05-15 16:45:21 -0300

    feat: 03/Arrays push & pop

--- 03-Javascript/Lessons/Arrays/script.js
+++ 03-Javascript/Lessons/Arrays/script.js
@@ -8,3 +8,17 @@ console.log(array);
 console.log(array2);
 // array and array2 are referencing the same array in memory, so changes to one will affect the other.
 // To create a copy of the array, we can use the spread operator or the slice method.
+
+const arr = ["Homer", "Marge", "Bart", "Lisa", "Maggie"];
+
+let tamanho = arr.push("Apu");
+console.log(arr);
+console.log(tamanho);
+
+tamanho = arr.unshift("Apu");
+console.log(arr);
+console.log(tamanho);
+
+const lastElement = arr.pop();
+console.log(arr);
+console.log(lastElement);
