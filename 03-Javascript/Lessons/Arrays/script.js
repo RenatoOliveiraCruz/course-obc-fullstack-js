f7d7694fcad37d1d0292a327e8f46a5efa92fb35
RenatoOliveiraCruz <renatoj.oliveiracruz@gmail.com>
2026-05-15 11:44:02 -0300

    feat: 03/Arrays comments

--- 03-Javascript/Lessons/Arrays/script.js
+++ 03-Javascript/Lessons/Arrays/script.js
@@ -6,3 +6,5 @@ let array2 = array;
 array2[0] = 5;
 console.log(array);
 console.log(array2);
+// array and array2 are referencing the same array in memory, so changes to one will affect the other.
+// To create a copy of the array, we can use the spread operator or the slice method.
