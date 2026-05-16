ccc758279dad96b8077ed787adcb723174e6bf09
RenatoOliveiraCruz <renatoj.oliveiracruz@gmail.com>
2026-05-16 08:06:08 -0300

    feat: 03/EX7/Pilha de cartas verify

--- 03-Javascript/Exercicios/Exercicio-7/script.js
+++ 03-Javascript/Exercicios/Exercicio-7/script.js
@@ -48,3 +48,46 @@ do {
 } while (fila !== 3);
 
 // Pilha de cartas
+let pilha;
+let cartas = [];
+
+do {
+  let listaCartas = "";
+  for (let i = 0; i < cartas.length; i++) {
+    listaCartas += `${i + 1}º - ${cartas[i]}\n`;
+  }
+
+  pilha = parseInt(
+    prompt(
+      "Bem vindo a pilha de cartas\n" +
+        "Cartas na pilha:\n " +
+        (listaCartas || "Nenhuma carta na pilha\n") +
+        "\n\nDigite uma opção para prosseguir: \n" +
+        "1 - Adicionar carta \n" +
+        "2 - Remover carta \n" +
+        "3 - Sair",
+    ),
+  );
+  switch (pilha) {
+    case 1:
+      let novaCarta = prompt("Digite o nome da carta:");
+      cartas.push(novaCarta);
+      alert("Carta adicionada à pilha: " + novaCarta);
+      break;
+
+    case 2:
+      if (cartas.length > 0) {
+        let cartaRemovida = cartas.pop();
+        alert("Carta removida da pilha: " + cartaRemovida);
+      } else {
+        alert("Não há cartas na pilha para remover.");
+      }
+
+    case 3:
+      alert("Encerrando o sistema. Até mais!");
+      break;
+
+    default:
+      alert("Opção inválida. Por favor, escolha uma opção válida.");
+  }
+} while (pilha !== 3);
